295060dfdf9c9755c0088728d3d1d1417865a0ce
Volodymyr Kovalenko <komp215.16@gmail.com>
2021-11-07 12:16:46 +0200

    ?

---
+++ index.js
@@ -0,0 +1,49 @@
+class timeCounter {
+  constructor({ selector, targetDate }) {
+    this.targetDate = new Date(targetDate);
+    this.days = document.querySelector(`${selector} .value[data-value="days"]`);
+    this.hours = document.querySelector(
+      `${selector} .value[data-value="hours"]`
+    );
+    this.minutes = document.querySelector(
+      `${selector} .value[data-value="mins"]`
+    );
+    this.seconds = document.querySelector(
+      `${selector} .value[data-value="secs"]`
+    );
+  }
+  zero(value) {
+    return value < 10 ? `0${value}` : value;
+  }
+  countDown() {
+    const currentTime = new Date();
+    this.createValue(currentTime);
+  }
+  showTime() {
+    setInterval(() => this.countDown(), 1000);
+  }
+
+  createValue(currentTime) {
+    const time = this.targetDate - currentTime;
+    this.days.textContent = this.zero(Math.floor(time / (1000 * 60 * 60 * 24)));
+    this.hours.textContent = this.zero(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    this.minutes.textContent = this.zero(
+      Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    );
+    this.seconds.textContent = this.zero(
+      Math.floor((time % (1000 * 60)) / 1000)
+    );
+  }
+}
+
+const myTimer = new timeCounter({
+  selector: "#timer-1",
+  targetDate: "Jul 17, 2019",
+});
+
+function startTimer() {
+  myTimer.showTime();
+}
+startTimer();
